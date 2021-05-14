0be8c10876e87fac309fd276273c6575b7653429
Dante Allievi <toto99.-@hotmail.com>
2021-05-14 16:57:38 +0200

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <>
+      <h1 className='header'>Hangman</h1>
+      <p className='header'>Find the hidden word - Enter a letter</p>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Notification.js
@@ -0,0 +1,9 @@
+const Notification = () => {
+  return (
+    <div class='notification-container' id='notification-container'>
+      <p>You have already entered this letter</p>
+    </div>
+  );
+};
+
+export default Notification;
